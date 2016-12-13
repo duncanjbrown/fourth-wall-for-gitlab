a5b7f3c76cb121252d81d824aca74c87cc795c67
Duncan Brown <duncan.brown@dxw.com>
2016-12-13 12:29:14 +0000

    (chore) Add GitLabRepo file
    Cloned from the corresponding GitHub file, and will be amended

---
+++ javascript/gitlab/gitlab-repo.js
@@ -0,0 +1,39 @@
+(function () {
+  "use strict";
+  window.FourthWall = window.FourthWall || {};
+
+  FourthWall.GitLabRepo = Backbone.Model.extend({
+    defaults: {
+      'baseUrl': FourthWall.getRepositoryEndpoint()
+    },
+
+    initialize: function () {
+      this.master = new FourthWall.GitLabMasterStatus({
+        baseUrl: this.get('baseUrl'),
+        projectId: this.get('projectId'),
+        sha: this.get('sha')
+      });
+
+      // this.master.on('change:failed', function () {
+      //   this.trigger('change');
+      // }, this);
+      //
+      // this.pulls = new FourthWall.GitHubPulls([], {
+      //   baseUrl: this.get('baseUrl'),
+      //   userName: this.get('userName'),
+      //   repo: this.get('repo'),
+      //   important: this.get('important')
+      // });
+      //
+      // this.pulls.on('reset add remove', function () {
+      //   this.trigger('change');
+      // }, this);
+    },
+
+    fetch: function () {
+      // this.pulls.fetch();
+      this.master.fetch();
+    }
+
+  });
+}());
