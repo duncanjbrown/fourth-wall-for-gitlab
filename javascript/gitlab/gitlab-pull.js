4634ba76d65ce8195a10ac42b226f655c1a47d87
Duncan Brown <duncan.brown@dxw.com>
2016-12-14 17:31:43 +0000

    (chore) Clean out commented code

--- javascript/gitlab/gitlab-pull.js
+++ javascript/gitlab/gitlab-pull.js
@@ -5,15 +5,6 @@
   FourthWall.GitLabPull = Backbone.Model.extend({
     initialize: function () {
       this.set('repo', this.collection.repo);
-      // this.comment = new FourthWall.GitHubComment();
-      // this.comment.url = this.get('comments_url');
-      // this.on('change:comments_url', function () {
-      //   this.comment.url = this.get('comments_url');
-      //   this.comment.fetch();
-      // }, this);
-      // this.comment.on('change', function () {
-      //   this.trigger('change');
-      // }, this);
       this.status = new FourthWall.GitLabStatus({
         baseUrl: this.collection.baseUrl,
         repo: this.get('repo'),
@@ -25,25 +16,11 @@
       this.status.on('change', function () {
         this.trigger('change');
       }, this);
-      // this.info = new FourthWall.GitHubInfo({
-      //   baseUrl: this.collection.baseUrl,
-      //   userName: this.collection.userName,
-      //   repo: this.get('repo'),
-      //   pullId: this.get('number')
-      // }),
-      // this.on('change:head', function () {
-      //   this.info.set('sha', this.get('head').sha);
-      // }, this);
-      // this.info.on('change', function () {
-      //   this.trigger('change');
-      // }, this);
       this.fetch();
     },
 
     fetch: function () {
       this.status.fetch();
-      // this.comment.fetch();
-      // this.info.fetch();
     },
 
     getAuthorName: function() {
